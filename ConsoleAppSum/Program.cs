d9e43b7d900a8b6296968afca1b6c51bb5178142
ZakharovIvan <ipz221_zia@student.ztu.edu.ua>
2024-01-27 03:27:58 +0200

    added ConsoleAppSum
    created a simple console application for calculating the sum of two numbers

---
+++ ConsoleAppSum/Program.cs
@@ -0,0 +1,23 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("перше число:");
+        string input1 = Console.ReadLine();
+
+        Console.WriteLine("друге число:");
+        string input2 = Console.ReadLine();
+
+        if (double.TryParse(input1, out double number1) && double.TryParse(input2, out double number2))
+        {
+            double sum = number1 + number2;
+            Console.WriteLine($"Сума двох чисел: {sum}");
+        }
+        else
+        {
+            Console.WriteLine("Введено некоректні числа. Будь ласка, введіть числа коректно.");
+        }
+
+        Console.ReadLine();
+    }
+}
